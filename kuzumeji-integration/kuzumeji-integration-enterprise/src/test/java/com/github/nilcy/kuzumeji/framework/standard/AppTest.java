56f3f3f568080c18f32f916609240842adace778
nilcy <nilcy@mac.com>
2014-02-01 21:25:17 +0900

    OPEN - task 1: プロジェクトの基本構造を作ること。 http://github.com/nilcy/kuzumeji/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ kuzumeji-integration/kuzumeji-integration-enterprise/src/test/java/com/github/nilcy/kuzumeji/framework/standard/AppTest.java
@@ -0,0 +1,19 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.github.nilcy.kuzumeji.framework.standard;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+/**
+ * @author nilcy
+ */
+@SuppressWarnings("all")
+public class AppTest {
+    @Test
+    public void test() {
+        assertThat("testee", is("testee"));
+    }
+}
