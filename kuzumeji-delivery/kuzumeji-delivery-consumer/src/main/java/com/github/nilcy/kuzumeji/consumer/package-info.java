df8e9b9b7a698aec3d5b12f2008320c698af2af4
nilcy <nilcy@mac.com>
2014-02-01 22:21:55 +0900

    OPEN - task 1: プロジェクトの基本構造を作ること。 http://github.com/nilcy/kuzumeji/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ kuzumeji-delivery/kuzumeji-delivery-consumer/src/main/java/com/github/nilcy/kuzumeji/consumer/package-info.java
@@ -0,0 +1,10 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+/**
+ * Master Domain | Screen Module
+ * @author nilcy
+ */
+package com.github.nilcy.kuzumeji.consumer;
